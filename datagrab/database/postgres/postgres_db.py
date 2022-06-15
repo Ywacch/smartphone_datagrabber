2d1f9fb0f50f998c4983f2505930446e031be296
ywacch <adebayolashe@gmail.com>
2022-06-15 15:36:38 -0600

    postgres code functionality attempted fix

--- datagrab/database/postgres/postgres_db.py
+++ datagrab/database/postgres/postgres_db.py
@@ -29,6 +29,7 @@ class Database:
         try:
             with self.connection:
                 with self.connection.cursor() as cursor:
+                    datagrab_log.debug(f'Attempting to execute "{statement}"')
                     cursor.execute(statement)
         except Exception as e:
             datagrab_log.error(e)
