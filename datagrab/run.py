9b365e11f20854cd0aab575db7d954fc186b298e
ywacch <adebayolashe@gmail.com>
2022-06-15 18:30:12 -0600

    code is fully functional now

--- datagrab/run.py
+++ datagrab/run.py
@@ -1,4 +1,4 @@
 from datagrab.datasources.ebay import ebay_pipeline as ebay_pipeline
 
 if __name__ == '__main__':
-    ebay_pipeline.start_pipeline(delete_temp_files=False, ebay_page_reach=1)
+    ebay_pipeline.start_pipeline(delete_temp_files=False, ebay_page_reach=35)
